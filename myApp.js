2f1be45f0308918943290e57e90507e21aace6e8
Tiaan <91896067+Tiaan-van-Staden@users.noreply.github.com>
2022-06-06 12:59:36 +0000

    task3-5

--- myApp.js
+++ myApp.js
@@ -1,9 +1,15 @@
 let express = require('express');
 let app = express();
 
-app.get(server,function (req, res) {
-    res.send('hello express');
-} )
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/json', function(req, res){
+  res.json({"message": "Hello json"})
+})
+
+app.use("/public", express.static(__dirname + "/public"))
 
 console.log("hello world");
 
