84f4fe62e14a8225bb49fa45967ff2c2d67ea262
Tiaan <91896067+Tiaan-van-Staden@users.noreply.github.com>
2022-06-07 09:19:18 +0000

    another question

--- myApp.js
+++ myApp.js
@@ -1,17 +1,34 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
+let message = {message : "Hello json"};
+const mySecret = process.env['MESSAGE_STYLE']
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html')
 })
 
 app.get('/json', function(req, res){
-  res.json({"message": "Hello json"})
+  if(process.env['MESSAGE_STYLE'] === "uppercase")
+  {
+    res.json({"message": "HELLO JSON"});
+  }else{
+    res.json(message);
+  }
 })
+
+app.use(function middleware(req, res, next) {
+  // Do something
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string)
+  // Call the next function in line:
+  next();
+});
 
 app.use("/public", express.static(__dirname + "/public"))
 
 console.log("hello world");
+
 
 
 
