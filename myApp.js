e5f7b94287970b81c588e61377dad2db4a11ef3e
Tiaan <91896067+Tiaan-van-Staden@users.noreply.github.com>
2022-06-08 12:05:31 +0000

    last question of stage 2

--- myApp.js
+++ myApp.js
@@ -1,8 +1,44 @@
 require('dotenv').config()
+var bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 let message = {message : "Hello json"};
-const mySecret = process.env['MESSAGE_STYLE']
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(function middleware(req, res, next) {
+  // Do something
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string)
+  // Call the next function in line:
+  next();
+});
+
+app.use("/public", express.static(__dirname + "/public"))
+
+app.post('/name', (req, res) => {
+  var firstName = req.query.first;
+  var lastName = req.query.last
+  req.body
+  res.json({name: `${firstName} ${lastName}`})
+})
+
+console.log("Aww shiet, here we go again");
+
+app.get('/now', function(req, res, next){
+  req.time = new Date().toString()
+  next();
+}, (req, res) => {
+  res.send({time: req.time})
+})
+
+app.get('/:word/echo', function(req, res, next){
+  req.params.word
+  next();
+}, (req, res) => {
+  res.send({echo: req.params.word})
+})
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html')
@@ -17,17 +53,11 @@ app.get('/json', function(req, res){
   }
 })
 
-app.use(function middleware(req, res, next) {
-  // Do something
-  var string = req.method + " " + req.path + " - " + req.ip;
-  console.log(string)
-  // Call the next function in line:
-  next();
-});
 
-app.use("/public", express.static(__dirname + "/public"))
 
-console.log("hello world");
+
+
+
 
 
 
